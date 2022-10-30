fe42d21d91ec2e26e4879f52500a79f3a2314721
Jelmer van Ofwegen <43699906+offie321@users.noreply.github.com>
2022-10-30 21:52:56 +0100

    added startScherm
    Op het startscherm kan de escaperoom worden gestart en hierbij word ook de hp waarde op 100 gezet. Als het spel is gestart wordt de speler meteen geredirect naar de huis.html pagina, waar de speler ook op uitkomt als de escaperoom wordt gestart.

---
+++ scripts/redirectIfStarted.js
@@ -0,0 +1,14 @@
+let gameStarted = localStorage.getItem('gameStarted');
+
+// Als de escaperoom is gestart is het niet meer mogelijk om op het startscherm te komen.
+// Waardoor de hp bar dus niet midden in de escaperoom kan worden gereset
+if(gameStarted == "true"){ 
+    location.replace("huis.html")
+};
+
+// Hier worden alle waardes die binnen de localstorage moeten worden opgeslagen vanaf de start aangemaakt
+function startEscapeRoom(){
+    localStorage.setItem('hpBar', '100');
+    localStorage.setItem('gameStarted', true)
+    window.location.href = "huis.html";
+}
