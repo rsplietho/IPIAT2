6e2cc18fb84b2a08c61646f536d5ab87ca77db17
Jelmer van Ofwegen <43699906+offie321@users.noreply.github.com>
2022-10-31 15:33:58 +0100

    added comments to notification.js

--- scripts/notification.js
+++ scripts/notification.js
@@ -1,15 +1,19 @@
 const healthpoints = localStorage.getItem('hpBar')
+const time = localStorage.getItem('time')
 
     Notification.requestPermission().then(perm =>{
         if (perm === 'granted') {
 
+            // notificatie  
             if(hp == "50")
+            // Boss is de titel
             new Notification('Boss', {
+                // bericht binnen de notificatie
                 body: "PAS OP DE POLITIE IS AL ONDERWEG",
+                // afbeelding binnen de notificatie
                 icon: "assets/JelmerO/icons8-boss-96.png",
             })
 
-
-
+            
         }
     });
