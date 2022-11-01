5a41ff415deb775de572e82802912e3289b587db
JelmerBankras <s1133373@student.hsleiden.nl>
2022-11-01 22:12:35 +0100

    timer button

---
+++ scripts/showBtn.js
@@ -0,0 +1,6 @@
+setTimeout(function () {
+    document.getElementById("apijl").style.visibility = "visible"
+  }, 15000);
+  setTimeout(function () {
+    document.getElementById("apijl").style.visibility = "visible"
+  }, 15000);
---
+++ scripts/showBtn2.js
@@ -0,0 +1,6 @@
+setTimeout(function () {
+    document.getElementById("abcpijl").style.visibility = "visible"
+  }, 20000);
+  setTimeout(function () {
+    document.getElementById("apijl").style.visibility = "visible"
+  }, 20000);
