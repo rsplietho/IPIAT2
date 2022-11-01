00b3fc87ccea8b08490e729f9380dd0f917f0f57
JelmerBankras <s1133373@student.hsleiden.nl>
2022-11-01 16:06:50 +0100

    puzzel af

--- scripts/puzzelJelmerB.js
+++ scripts/puzzelJelmerB.js
@@ -10,4 +10,19 @@ Button1.addEventListener("click", function(e) {
 Button2.addEventListener("click", function(e) {
     document.getElementById("code").innerHTML = "05";
     Button2.style.display = "none";
+    Button3.style.display = "block";
+});
+Button3.addEventListener("click", function(e) {
+    document.getElementById("code").innerHTML = "052";
+    document.getElementById("confirm").style.display = "block"
+    document.getElementsByClassName("button_alarm1").style.display = "none"
+    document.getElementsByClassName("button_alarm2").style.display = "none"
+    document.getElementsByClassName("button_alarm3").style.display = "none"
+    document.getElementsByClassName("button_alarm4").style.display = "none"
+    document.getElementsByClassName("button_alarm5").style.display = "none"
+    document.getElementsByClassName("button_alarm6").style.display = "none"
+    document.getElementsByClassName("button_alarm7").style.display = "none"
+    document.getElementsByClassName("button_alarm8").style.display = "none"
+    document.getElementsByClassName("button_alarm9").style.display = "none"
+    document.getElementsByClassName("button_alarm10").style.display = "none"
 });
