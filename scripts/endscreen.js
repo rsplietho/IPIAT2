fdd2a740fc180de9cd8a9c228ea6918ec4c15f1c
Jelmer van Ofwegen <43699906+offie321@users.noreply.github.com>
2022-11-02 02:47:13 +0100

    added restartEscaperoom() to endscreen

--- scripts/endscreen.js
+++ scripts/endscreen.js
@@ -8,3 +8,8 @@ const t = 600 - time;
 timeLeft.innerHTML = t
 
 showTime.innerHTML = time
+
+function restartEscapeRoom(){
+    localStorage.setItem('gameStarted', false)
+    window.location.href = "index.html";
+}
